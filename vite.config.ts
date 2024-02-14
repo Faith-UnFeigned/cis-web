4821850e9bd25841d2ed04777448cc6c4be8cda4
Olivier JM Maniraho <manolivier93@gmail.com>
2024-02-14 07:54:08 +0200

    setup tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    css: {
-        modules: {
-            localsConvention: "camelCase",
-        },
+  plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
     },
+  },
+  server: {
+    open: true,
+  },
 });
