9a37caca651c38764248296ce3dd89250f6029f6
Olivier JM Maniraho <manolivier93@gmail.com>
2026-02-15 12:22:17 +0200

    Remove unused types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,15 +1,12 @@
 export interface HymnLyricBlock {
-    type: "verse" | "refrain";
-    index?: number;
-    lines: string[];
+  type: "verse" | "refrain";
+  index?: number;
+  lines: string[];
 }
 
 export interface Hymn {
-    number: number;
-    title: string;
-    index?: string;
-    title_english?: string;
-    hymnal_references?: string;
-    revision?: number;
-    lyrics: HymnLyricBlock[];
+  number: number;
+  title: string;
+  index?: string;
+  lyrics: HymnLyricBlock[];
 }
